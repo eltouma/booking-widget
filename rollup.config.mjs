cdea6eac38e011164dc8b52cb7965921e036b3d8
User <user@mbp-de-user.home>
2025-10-05 10:21:18 +0200

    Fix styles on host website

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,37 @@
 import babel from '@rollup/plugin-babel';
-import typescript from '@rollup/plugin-typescript';
-import postcss from 'rollup-plugin-postcss';
-import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import { parseArgs } from 'node:util';
+import postcss from 'rollup-plugin-postcss';
+import tsConfigPaths from 'rollup-plugin-tsconfig-paths';
 
-const production = process.env.NODE_ENV === 'production';
+const args = parseArgs({
+  options: {
+    environnement: {
+      type: 'string',        
+      short: 'e',
+      default: 'development',
+    },
+    configuration: {
+      type: "string",
+      short: 'c',
+    },
+  },
+});
+
+const env = args.values.environment || 'development';
+const production = env == 'production';
+let environmentVariatblePath = './env.development';
+
+console.log(`Building widget for ${env} environment...`);
+
+if (production) {
+  environmentVariatblePath = './.env.production';
+}
+
 const fileName = 'booking_widget.js';
 
 export default {
@@ -14,31 +39,60 @@ export default {
   output: {
     file: `dist/${fileName}`,
     format: 'iife',
-    name: 'BookingWidget', // accessible via window.BookingWidget si besoin
+    name: 'BookingWidget',
     sourcemap: false,
     inlineDynamicImports: true,
-    globals: {
-      react: 'React',
-      'react-dom': 'ReactDOM',
-      'react-dom/client': 'ReactDOM',
-    },
   },
-  external: ['react', 'react-dom'], // React/CDN externe
+  external: [],
   plugins: [
+    tsConfigPaths({
+      tsConfigPath: './tsconfig.json',
+    }),
     replace({
       preventAssignment: true,
       'process.env.NODE_ENV': JSON.stringify(production ? 'production' : 'development'),
     }),
-    typescript({ tsconfig: './tsconfig.json' }),
-    nodeResolve({ browser: true, extensions: ['.ts', '.tsx', '.js', '.jsx'] }),
-    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+    }),
+    nodeResolve({
+      extensions: ['.tsx', '.ts', '.json', '.js', '.jsx', '.mjs'],
+      browser: true,
+      dedupe: ['react', 'react-dom'],
+    }),
     babel({
       babelHelpers: 'bundled',
-      extensions: ['.ts', '.tsx', '.js', '.jsx'],
-      presets: ['@babel/preset-typescript', ['@babel/preset-react', { runtime: 'automatic' }]],
+      presets: [
+        '@babel/preset-typescript',
+        [
+          '@babel/preset-react',
+          {
+            runtime: 'automatic',
+            targets: '>0.1%, not dead, not op_mini all',
+          },
+        ],
+      ],
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.mjs'],
     }),
-    postcss({ extensions: ['.css'], extract: true, minimize: production }),
-    terser(),
+    postcss({
+      extensions: ['.css'],
+      inject: false,
+      extract: false,
+      minimize: true,
+      toString: true,
+    }),
+    commonjs(),
+    terser({
+      ecma: 2020,
+      mangle: { toplevel: true },
+      compress: {
+        module: true,
+        toplevel: true,
+        unsafe_arrows: true,
+        drop_console: true,
+        drop_debugger: true,
+      },
+      output: { quote_style: 1 },
+    }),
   ],
 };
-
